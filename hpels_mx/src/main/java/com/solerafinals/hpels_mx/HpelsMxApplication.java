f00fc44b699f4e92d9bfa6e2d32af8ffbe3b05cb
José Azevedo <jose.azevedo@solera.com>
2022-11-17 09:38:24 +0100

    add Backend Folder

---
+++ hpels_mx/src/main/java/com/solerafinals/hpels_mx/HpelsMxApplication.java
@@ -0,0 +1,13 @@
+package com.solerafinals.hpels_mx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpelsMxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HpelsMxApplication.class, args);
+	}
+
+}
---
+++ hpels_mx/src/test/java/com/solerafinals/hpels_mx/HpelsMxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solerafinals.hpels_mx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpelsMxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
