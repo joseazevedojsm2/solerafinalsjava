3dd23653aff50f1c974ed5fa3f6bf47bb3055402
José Azevedo <jose.azevedo@solera.com>
2022-11-17 11:03:02 +0100

    add Update and Delete Reqs

--- hpels_mx/src/main/java/com/solerafinals/hpels_mx/controller/UserInfoController.java
+++ hpels_mx/src/main/java/com/solerafinals/hpels_mx/controller/UserInfoController.java
@@ -5,9 +5,11 @@ import com.solerafinals.hpels_mx.service.UserInfoService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +35,15 @@ public class UserInfoController {
     @GetMapping("/{id}")
     public UserInfo retrieveUserInfoById(@PathVariable int id){
         return service.getUserInfoById(id);
+    }
+
+    @PutMapping("/{id}")
+    public UserInfo updateUserInfoById(@PathVariable int id, @RequestBody UserInfo user){
+        return service.updateUserInfo(id,user);
+    }
+
+    @DeleteMapping("/{id}")
+    public Boolean deleteUserInfoByInfo(@PathVariable int id){
+        return service.deleteUserInfo(id);
     }
 }
--- hpels_mx/src/main/java/com/solerafinals/hpels_mx/service/UserInfoService.java
+++ hpels_mx/src/main/java/com/solerafinals/hpels_mx/service/UserInfoService.java
@@ -16,9 +16,16 @@ public class UserInfoService {
         this.repository = repository;
     }
 
-    public UserInfo createUserInfo(UserInfo user){
+    public boolean userValidation(UserInfo user){
         if(user.getFirst_name().isEmpty() || user.getLast_name().isEmpty()
-            || user.getEmail().isEmpty() || user.getPhone_number()<900000000)
+                || user.getEmail().isEmpty() || user.getPhone_number()<900000000)
+            return false;
+
+        return true;
+    }
+
+    public UserInfo createUserInfo(UserInfo user){
+        if(!userValidation(user))
             return null;
 
         UserInfo newUser = repository.save(user);
@@ -44,5 +51,38 @@ public class UserInfoService {
             return null;
 
         return user.get();
+    }
+
+    public UserInfo updateUserInfo(int id, UserInfo userInfo){
+        Optional<UserInfo> user = repository.findById(id);
+
+        if(!user.isPresent())
+            return null;
+
+        if(!userValidation(userInfo))
+            return null;
+
+        user.get().setEmail(userInfo.getEmail());
+        user.get().setFirst_name(userInfo.getFirst_name());
+        user.get().setLast_name(userInfo.getLast_name());
+        user.get().setPhone_number(userInfo.getPhone_number());
+
+        return repository.save(user.get());
+    }
+
+    public Boolean deleteUserInfo(int id){
+        Optional<UserInfo> user = repository.findById(id);
+
+        if(!user.isPresent())
+            return null;
+
+        repository.deleteById(id);
+
+        user = repository.findById(id);
+        if (user.isPresent())
+            return false;
+
+        return true;
+
     }
 }
